6dda99cda76131eb76e28964606df4d173921282
Lilibeth <lilibeth.sequeiraj@gmail.com>
2022-02-13 21:58:17 -0600

    feat(search): initial styles

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,65 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <section class="section">
+      <nav class="nav has-shadow">
+        <div class="field is-grouped">
+          <p class="control is-expanded">
+            <input type="text" class="input is-large" placeholder="Buscar canciones" v-model="searchQuery">
+          </p>
+          <p class="control">
+            <a class="button is-info is-large" @click="search">Buscar</a>
+          </p>
+          <p class="control">
+            <a href="" class="button is-danger is-large">&times;</a>
+          </p>
+          <p>
+            <small>{{searchMessage}}</small>
+          </p>
+        </div>
+      </nav>
+
+      <div class="container">
+        <div class="colums">
+          <div class="column" v-for="(track, index) in tracks" :key="index">
+            {{track.name}} - {{track.artist}}
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
+<script>
+const tracks = [
+  { name: 'Muchacha', artist: 'Luis Alberto Spinetta' },
+  { name: 'Hoy aca en el baile', artist: 'El Pepo' },
+  { name: 'I was made for loving you', artist: 'Kiss' }
+]
+
+export default {
+  name: 'app',
+
+  data () {
+    return {
+      searchQuery: '',
+      tracks: []
+    }
+  },
+
+  computed: {
+    searchMessage () {
+      return `Encontrados: ${this.tracks.length}`
+    }
+  },
+
+  methods: {
+    search () {
+      this.tracks = tracks
+    }
+  }
+}
+</script>
+
 <style lang="scss">
-@import './scss/main.scss'
+@import './scss/main.scss';
 </style>
